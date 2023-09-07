c55e701682c55e0cf262743d1ab742fbd10c8fe7
beckboan <131830752+beckboan@users.noreply.github.com>
2023-09-07 19:09:02 -0400

    CMake setup

---
+++ build/CMakeFiles/2dphysics.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "2dphysics"
+  "2dphysics.pdb"
+  "CMakeFiles/2dphysics.dir/main.cpp.o"
+  "CMakeFiles/2dphysics.dir/main.cpp.o.d"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/2dphysics.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
