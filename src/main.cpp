bc7eb40d3eaf3bf843fdc9f50e05e0c7fab5cf49
beckboan <131830752+beckboan@users.noreply.github.com>
2023-09-07 20:31:38 -0400

    Testing of new functions

--- src/main.cpp
+++ src/main.cpp
@@ -2,8 +2,10 @@
 #include "vec2d.h"
 
 int main(int, char**){
-    vec2d vector1(3.12,4.12);
+    vec2d vector1(2.00,2.00);
     vec2d vector2(2.00, 1.00);
+    auto product = dp(vector1, vector2);
+    std::cout << product << std::endl;
     
-    std::cout << vector1.x << ", " << vector1.y << std::endl;
+    //std::cout << vector1.x << ", " << vector1.y << std::endl;
 }
