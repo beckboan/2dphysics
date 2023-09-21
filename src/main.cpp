b76d87c301d8dd48796e5788dc587d60268b2689
beckboan <131830752+beckboan@users.noreply.github.com>
2023-09-20 22:05:22 -0400

    Draw doesn't work but progress

--- src/main.cpp
+++ src/main.cpp
@@ -7,6 +7,39 @@
 
 #define DBG(msg) std::cout << msg <<std::endl;
 
+void drawCircle(SDL_Renderer * renderer, vec2d& position, float radius) 
+{ 
+    float diameter = radius*2;
+    float x = (radius - 1);
+    int y = 0;
+    int dx = 1;
+    int dy = 1;
+    int err = dx - diameter;
+
+    while (x >= y) {
+        SDL_RenderDrawPoint(renderer, position.x + x, position.y - y);
+        SDL_RenderDrawPoint(renderer, position.x + x, position.y + y);
+        SDL_RenderDrawPoint(renderer, position.x - x, position.y - y);
+        SDL_RenderDrawPoint(renderer, position.x - x, position.y + y);
+        SDL_RenderDrawPoint(renderer, position.x + y, position.y - x);
+        SDL_RenderDrawPoint(renderer, position.x + y, position.y + x);
+        SDL_RenderDrawPoint(renderer, position.x - y, position.y - x);
+        SDL_RenderDrawPoint(renderer, position.x - y, position.y + x);
+
+        if (err <= 0) {
+            ++y;
+            err+= dy;
+            dx +=2;
+        }
+
+        if (err > 0) {
+            --x;
+            dx +=2;
+            err += (dx-diameter);
+        }
+    }
+}
+
 int main(int, char**){
     PhysicsWorld world;
 
@@ -24,8 +57,7 @@ int main(int, char**){
     SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, 500, 500, SDL_WINDOW_OPENGL);
     
     SDL_Renderer* renderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_ACCELERATED);
-
-    shp->draw(renderer, position);
+    drawCircle(renderer, position, 5);
 
     std::cin.get();
 }
