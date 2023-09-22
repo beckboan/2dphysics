d8d8536216cf8f0cc865df744587232e0252a39c
beckboan <131830752+beckboan@users.noreply.github.com>
2023-09-21 21:27:57 -0400

    EOD updates

--- src/main.cpp
+++ src/main.cpp
@@ -15,13 +15,13 @@ int main(int, char**){
     // world.addCircle(1, position, 1000);
     // world.addRect(1, 1, position, 1000);
 
-    std::unique_ptr<Shape> shp = std::make_unique<Circle>(250);
+    std::unique_ptr<Shape> shp = std::make_unique<Circle>(2);
     std::shared_ptr<RigidBody> bod = std::make_shared<RigidBody>(shp,position,1000);
     bod->moveto(vec2d(500,500));
     SDL_Init(SDL_INIT_EVERYTHING);
 
     SDL_Window *window = SDL_CreateWindow("OpenGL Test",
-    SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, 1000, 1000, SDL_WINDOW_OPENGL);
+    SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, 1800, 1200, SDL_WINDOW_OPENGL);
     
     SDL_Renderer* renderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_ACCELERATED);
     SDL_SetRenderDrawColor(renderer, 0, 0, 0, 255);
