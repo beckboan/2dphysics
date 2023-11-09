3df68ef6dd939355495949cb749b485a58baa77b
Beck Boan <beckmboan@gmail.com>
2023-11-08 22:15:11 -0500

    Added virtual function overrides for edge

--- src/edge.cpp
+++ src/edge.cpp
@@ -1,4 +1,24 @@
+#include "edge.h"
+
+Edge::Edge(std::vector<vec2d> &verticies) {}
 
 Shape::ShapeType Edge::getType() const { return ShapeType::Poly; }
 
 std::string Edge::getName() const { return "Poylgon"; }
+
+void Edge::calculateMassProperties(float density) {
+
+  std::shared_ptr<RigidBody> body_ref = body.lock();
+  vec2d position = body_ref->position;
+  body_ref->area = 0;
+  body_ref->m = 0;
+  body_ref->inv_m = 0;
+  body_ref->I = 0;
+  body_ref->inv_I = 0;
+}
+
+void Edge::createAABB() {
+
+  aabb.setMax(0, 0);
+  aabb.setMin(0, 0);
+}
