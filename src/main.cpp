539159410e4bfcfc1fa6420c0803674e41a3e7b2
Beck Boan <beckmboan@gmail.com>
2023-11-28 12:58:17 -0500

    Changed some parameters to floats

--- src/main.cpp
+++ src/main.cpp
@@ -5,10 +5,10 @@
 
 int main(int argc, char *args[]) {
   Engine engine;
-  engine.createWorld(9.8f);
+  engine.createWorld(9.8);
 
-  vec2d position = vec2d(0, 0);
-  float density = 1000;
+  vec2d position = vec2d(0.0, 0.0);
+  float density = 1000.0;
   vec2d movetopos = vec2d(-300, 0);
 
   std::vector<vec2d> verticies = {vec2d(0, 0), vec2d(50, 0), vec2d(50, 50),
@@ -18,8 +18,8 @@ int main(int argc, char *args[]) {
                                     vec2d(0, 10), vec2d(15, 20)};
   // engine.m_world->addCircle(10, position, 1000, 1);
   // engine.m_world->addCircle(10, vec2d(0, 100), 1000, 0);
-  engine.m_world->addPoly(verticies, position, 1000, 1);
-  engine.m_world->addPoly(verticies, vec2d(0, 90), 1000, 0);
+  engine.m_world->addPoly(verticies, position, 10, 1);
+  engine.m_world->addPoly(verticies, vec2d(0.0, 90.0), 10, 0);
   // engine.m_world->addPoly(verticies, vec2d(0, 400), 1000, 0);
   // engine.m_world->addEdge(vec2d(-200, -400), vec2d(400, -400), 1);
   // engine.m_world->addCircle(10, vec2d(1, -100), 1000, 1);
